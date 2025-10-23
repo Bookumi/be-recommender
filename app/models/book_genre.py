b78ed6dd092ae4d09d5860277495c2e9db10934f
fadelanumah <fadelanumah@gmail.com>
2025-10-23 21:08:02 +0700

    fix(book-genre): fix foreign key name

--- app/models/book_genre.py
+++ app/models/book_genre.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Table, Column, Integer, ForeignKey
+from app.database import Base
 
 books_genres = Table(
   "books_genres",
-  Column("book_id", Integer, ForeignKey("book.id", ondelete="CASCADE"), primary_key=True),
-  Column("genre_id", Integer, ForeignKey("genre.id", ondelete="CASCADE"), primary_key=True),
+  Base.metadata, 
+  Column("book_id", Integer, ForeignKey("books.id", ondelete="CASCADE"), primary_key=True),
+  Column("genre_id", Integer, ForeignKey("genres.id", ondelete="CASCADE"), primary_key=True),
 )
