3b563aaae7cfc4601c3a5bf9c6b5933809c8b0cb
fadelanumah <fadelanumah@gmail.com>
2025-10-27 22:11:35 +0700

    chore(main): fix main to load all models in first start

---
+++ app/crud/__init__.py
@@ -0,0 +1,3 @@
+from app.models.book import Book
+from app.models.genre import Genre
+from app.models.book_genre import books_genres
--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.routers import book
+from app import models
+from app.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="recommender-service")
 
+app.include_router(book.router)
+
 @app.get("/")
 def root():
-  return {"message": "FastAPI service is running"}
+    return {"message": "FastAPI service is running"}
