f47a7cf10a4a47716f132edef8f24ae12a8d05e7
fadelanumah <fadelanumah@gmail.com>
2025-10-27 22:49:06 +0700

    feat(books): implement pagination

--- app/crud/book.py
+++ app/crud/book.py
@@ -1,5 +1,15 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from app.models.book import Book
+from app.models.pagination import Pagination
 
-def get_all_books(db: Session):
-  return db.query(Book).order_by(Book.score.desc())
+def get_all_books(db: Session, pagination: Pagination):
+  query = db.query(Book).options(joinedload(Book.genres)).order_by(Book.score.desc())
+  books = (
+    query
+    .offset(pagination.skip)
+    .limit(pagination.limit)
+    .all()
+  )
+  total = query.count()
+
+  return books, total
--- app/routers/book.py
+++ app/routers/book.py
@@ -2,10 +2,21 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas import book as BookSchema
+from app.schemas import pagination as PaginationSchema
+from app.models import pagination as PaginationModel
 from app.crud import book as BookCRUD
 
 router = APIRouter(prefix="/api/v1/books", tags=["Books"])
 
-@router.get("", response_model=list[BookSchema.BookResponse])
-def get_all_books(db: Session = Depends(get_db)):
-  return BookCRUD.get_all_books(db)
+@router.get("", response_model=PaginationSchema.PaginatedResponse[BookSchema.BookResponse])
+def get_all_books(
+  pagination: PaginationModel.Pagination = Depends(),
+  db: Session = Depends(get_db)
+):
+  books, total = BookCRUD.get_all_books(db, pagination)
+  return PaginationSchema.PaginatedResponse(
+    total=total,
+    page=pagination.page,
+    limit=pagination.limit,
+    items=books
+  )
