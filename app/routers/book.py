c3d1f2b6a5df62c643c0fe6fb2d9892d93dda89f
fadelanumah <fadelanumah@gmail.com>
2025-10-28 22:23:47 +0700

    feat: add base response

--- app/routers/book.py
+++ app/routers/book.py
@@ -1,31 +1,38 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.schemas import book as BookSchema
-from app.schemas import pagination as PaginationSchema
+from app.schemas.book import BookFilter, BookResponse
+from app.schemas.pagination import PaginatedResponse, Pagination
+from app.schemas.response import BaseResponse
 from app.crud import book as BookCRUD
 
 router = APIRouter(prefix="/api/v1/books", tags=["Books"])
 
-@router.get("", response_model=PaginationSchema.PaginatedResponse[BookSchema.BookResponse])
+@router.get("", response_model=BaseResponse[PaginatedResponse[BookResponse]])
 def get_all_books(
-  book_filter: BookSchema.BookFilter = Depends(BookSchema.BookFilter.as_query),
-  pagination: PaginationSchema.Pagination = Depends(),
+  book_filter: BookFilter = Depends(BookFilter.as_query),
+  pagination: Pagination = Depends(),
   db: Session = Depends(get_db)
 ):
   books, total = BookCRUD.get_all_books(db, pagination, book_filter)
-  return PaginationSchema.PaginatedResponse(
-    total=total,
-    page=pagination.page,
-    limit=pagination.limit,
-    items=books
+  return BaseResponse(
+    message="success get all books",
+    data=PaginatedResponse(
+      total=total,
+      page=pagination.page,
+      limit=pagination.limit,
+      items=books
+    )
   )
 
-@router.get("/{id}", response_model=BookSchema.BookResponse)
+@router.get("/{id}", response_model=BaseResponse[BookResponse])
 def get_detail_book(id: int, db: Session = Depends(get_db)):
   book = BookCRUD.get_detail_book(db, id)
 
   if not book:
     raise HTTPException(status_code=404, detail="book not found")
 
-  return book
+  return BaseResponse(
+    message="success get detail book",
+    data=book
+  )
---
+++ app/schemas/response.py
@@ -0,0 +1,8 @@
+from typing import Generic, TypeVar
+from pydantic.generics import GenericModel
+
+T = TypeVar("T")
+
+class BaseResponse(GenericModel, Generic[T]):
+  message: str
+  data: T
