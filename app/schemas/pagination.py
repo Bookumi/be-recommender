4682d458c3b0e1d5884eb0420554d5664f4df15d
fadelanumah <fadelanumah@gmail.com>
2025-10-28 21:09:38 +0700

    chore(pagination): refactor pagination schema

--- app/schemas/pagination.py
+++ app/schemas/pagination.py
@@ -1,8 +1,18 @@
 from typing import Generic, TypeVar, List
 from pydantic.generics import GenericModel
+from pydantic import BaseModel, Field, computed_field
+
+class Pagination(BaseModel):
+  page: int = Field(1, ge=1, description="current page number")
+  limit: int = Field(20, ge=1, le=100, description="maximum of item in each page")
+
+  @computed_field
+  @property
+  def skip(self) -> int:
+    # compute number of item to skip based on page and limit.
+    return (self.page - 1) * self.limit
 
 T = TypeVar("T")
-
 class PaginatedResponse(GenericModel, Generic[T]):
   total: int
   page: int
