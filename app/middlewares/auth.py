71607e4ead8ba25a2a17ebce160161af45976bac
fadelanumah <fadelanumah@gmail.com>
2025-11-04 14:45:09 +0700

    feat(middleware): add auth middleware

---
+++ app/middlewares/auth.py
@@ -0,0 +1,32 @@
+from fastapi import status, HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.schemas.auth import JWTPayload
+from dotenv import load_dotenv
+import os
+import jwt
+
+load_dotenv()
+security = HTTPBearer()
+
+def verify_auth_token(credentials: HTTPAuthorizationCredentials = Security(security)):
+  if not credentials:
+      raise HTTPException(
+          status_code=status.HTTP_403_FORBIDDEN,
+          detail="Authorization credentials missing"
+      )
+  
+  token = credentials.credentials[7:].strip()
+
+  try:
+    decoded_token = jwt.decode(
+        token,
+        key=os.getenv("SECRET_KEY"),
+        algorithms=[os.getenv("ALGORITHM") or "HS256"],
+        options={"verify_sub": False}
+    )
+
+    return JWTPayload(**decoded_token)    
+  except jwt.ExpiredSignatureError:
+      raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+  except jwt.InvalidTokenError as e:
+      raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid or malformed token: {str(e)}")
